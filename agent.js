72ebec2ff39be745e29bfabb20f8a20e2530c39b
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-06-21 23:13:08 +0200

    agent_loop_in_js

--- agent.js
+++ agent.js
@@ -1,29 +1,35 @@
 const { Configuration, OpenAIApi } = require("openai");
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function returnName() {
-    return "John";
-}
-
-async function createChatCompletion() {
+async function createChatCompletion(userInput, chatHistory = []) {
+  chatHistory.push(
+    {role: "system", "content": "You are the Polywrap Agent. Capable of interacting with any tool, service, or API that has a wrap hosted on the Polywrap Registry."},
+    {role: "user", content: userInput}
+  );
+  
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-0613",
-    messages: [
-      {role: "system", "content": "You are a helpful assistant."},
-      {role: "user", content: "What's my name?"}
-    ],
-    // functions: {
-    //     "name": "return_name",
-    //     "description": "Use this function every time you need to know the user's name.",
-    // },
-    // function_call: "auto"
-        
+    messages: chatHistory
   });
-  console.log(completion.data.choices[0].message);
+  chatHistory.push({role: "assistant", content: completion.data.choices[0].message.content});
+
+  console.log(completion.data.choices[0].message.content);
+
+  continueChat(chatHistory);
 }
 
-createChatCompletion().catch(console.error);
+function continueChat(chatHistory) {
+  readline.question('Human Feedback: ', userInput => {
+    createChatCompletion(userInput, chatHistory).catch(console.error);
+  });
+}
+
+continueChat([]);
