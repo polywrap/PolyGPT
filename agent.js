1e10a14fe384dfa2f1b68cd782d0db2118e068a5
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-06-21 22:53:28 +0200

    agent.js

--- agent.js
+++ agent.js
@@ -5,13 +5,23 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+async function returnName() {
+    return "John";
+}
+
 async function createChatCompletion() {
   const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-0613",
     messages: [
       {role: "system", "content": "You are a helpful assistant."},
-      {role: "user", content: "Hello world"}
+      {role: "user", content: "What's my name?"}
     ],
+    // functions: {
+    //     "name": "return_name",
+    //     "description": "Use this function every time you need to know the user's name.",
+    // },
+    // function_call: "auto"
+        
   });
   console.log(completion.data.choices[0].message);
 }
