3ae97ff90665c1e1967d85d4bee173c78a5294f0
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-06-22 15:21:14 +0200

    broken js agent with functionality structure

--- agent.js
+++ agent.js
@@ -12,14 +12,32 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const client = new PolywrapClient();
 
+uri_allowlist= [    
+    "ens/wraps.eth:ens-text-record-uri-resolver-ext@1.0.0",
+    "ens/wraps.eth:logger@1.0.0",
+    "ens/wraps.eth:uniswap-v3@1.0.0",
+]
+
 const functions = {
-    GetDateTime: () => new Date().toLocaleString(),
-    GetWrap: () => client.invoke({ uri: "ens/wraps.eth:logger@1.0.0", method: "log", args: { message: "Hello Polywrap!" }}).then(console.log),
-    GetENS: async () => {
+    GetToolLibrary: () => new Date().toLocaleString(),
+    GetFunctionsfromTool: async () => {
         const resolutionResult = await client.invoke({ uri: "ens/wraps.eth:ens-text-record-uri-resolver-ext@1.0.0", method: "tryResolveUri", args: { authority: "ens", path: "uniswap.wraps.eth:v3" }});
         console.log(resolutionResult.ok ? resolutionResult.value : resolutionResult.error);
+    },
+    InvokeWrap: (options) => {
+        console.log("Invoking wrap")
+        console.log(options)
+        client.invoke(options).then(console.log)
+        console.log("Invoked wrap")
+
     }
 };
+
+const functions_description = [
+    { name: "GetToolLibrary", description: "A function to get the current date and time", parameters: {type: "object", properties: {}}},
+    { name: "GetFunctionsfromTool", description: "A function to log something in the CLI, like a console log or print", parameters: {type: "object", properties: { }}},
+    { name: "InvokeWrap", description: "A function to get the ENS record", parameters: {type: "object", properties: {options: {type: "object", properties: {uri: {type: "string"}, method: {type: "string"}}}, required: ["uri", "method"]}}}
+];
 
 async function createChatCompletion(userInput, chatHistory = []) {
   chatHistory.push(
@@ -30,11 +48,7 @@ async function createChatCompletion(userInput, chatHistory = []) {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-0613",
     messages: chatHistory,
-    functions: [
-        { name: "GetDateTime", description: "A function to get the current date and time", parameters: {type: "object", properties: {}}},
-        { name: "GetWrap", description: "A function to get the Polywrap", parameters: {type: "object", properties: {}}},
-        { name: "GetENS", description: "A function to get the ENS record", parameters: {type: "object", properties: {}}}
-    ],
+    functions: functions_description,
     function_call:"auto"
   });
 
@@ -52,7 +66,7 @@ async function createChatCompletion(userInput, chatHistory = []) {
         chatHistory.push(...updatedChatHistory);
 
     } catch (error) {
-        let errorMessage = String(error);
+        let errorMessage = String(error.message);
         let updatedChatHistory = [
             { role: "assistant", content: `Error: ${errorMessage}` }
         ];
@@ -69,7 +83,7 @@ async function createChatCompletion(userInput, chatHistory = []) {
 
 function continueChat(chatHistory) {
   readline.question('Human Feedback: ', userInput => {
-      createChatCompletion(userInput, chatHistory).catch(console.error);
+      createChatCompletion(userInput, chatHistory).catch( (error) => console.error(error.message));
       console.log(chatHistory);
     
   });
