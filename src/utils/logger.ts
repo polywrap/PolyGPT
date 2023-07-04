bcf9d6c02665193457cf97a78892e96bc6303182
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-07-04 18:14:05 +0200

    chore: datetime on chatlogs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,22 @@
 import { ChatCompletionRequestMessage } from "openai";
 import winston from "winston";
 
+const getLogFileName = () => {
+  // get current date
+  const date = new Date();
+  
+  // format the date to your liking
+  const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+  
+  // use the formatted date in the filename
+  return `chat_${formattedDate}.log`;
+}
+
 const logger = winston.createLogger({
   format: winston.format.simple(),
   transports: [
-    new winston.transports.File({ filename: 'chat.log' }),
+    // use the function to get the filename
+    new winston.transports.File({ filename: getLogFileName() }),
   ],
 });
 
