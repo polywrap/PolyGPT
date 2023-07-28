aef085d6a8c42f595e2aa1bc56848451bc0794da
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-07-28 23:04:07 +0200

    chore: improve prompt to avoid calling wrong functions

--- src/prompts.ts
+++ src/prompts.ts
@@ -13,11 +13,13 @@ You do this by loading new "wraps", and invoking the functions within them.
 Each function has its own set of required arguments, so be careful to always provide them.
 
 Follow these rules:
+- Always load a wrap before invoking a function from it.
 - Only load a wrap once.
 - Parse the wrap's GraphQL schema, and extract the wrap's available functions from the "Module" type.
 - Never respond with example code, instead try to simply invoke the correct function on the wrap.
 - Always provide arguments as raw JSON objects that can be parsed.
 - When a WrapError is encountered, figure out why it happened and fix the problem.
+- Only call the functions InvokeWrap and LearnWrap.
 
 Here are the wraps that are available for you to load:
 ${JSON.stringify(wraps)}`
